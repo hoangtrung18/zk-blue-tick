06aeffead98e0318adc4d087bf81f0b5ca98dc1e
Dung Le <dunglh@decentralab.asia>
2023-03-03 19:09:14 +0700

    Add test case for test call approval account

--- integration-tests/src/main.ava.ts
+++ integration-tests/src/main.ava.ts
@@ -13,12 +13,14 @@ test.beforeEach(async (t) => {
   // Deploy contract
   const root = worker.rootAccount;
   const contract = await root.createSubAccount("test-account");
+  const alice = await root.createSubAccount("alice");
+  const bob = await root.createSubAccount("bob");
   // Get wasm file path from package.json test script in folder above
   await contract.deploy(process.argv[2]);
 
   // Save state for test runs, it is unique for each test
   t.context.worker = worker;
-  t.context.accounts = { root, contract };
+  t.context.accounts = { root, contract, bob, alice };
   root.call(contract, "init", { owner_id: root.accountId });
 });
 
@@ -41,9 +43,30 @@ test("Init fee", async (t) => {
   t.is(fee, "2");
 });
 
-// test('changes the message', async (t) => {
-//   const { root, contract } = t.context.accounts;
-//   await root.call(contract, 'set_greeting', { message: 'Howdy' });
-//   const message: string = await contract.view('get_greeting', {});
-//   t.is(message, 'Howdy');
-// });
+test("Call set operator", async (t) => {
+  const { root, contract, alice } = t.context.accounts;
+  const res = await root.callRaw(contract, "set_operator", {
+    operator_address: alice.accountId,
+    value: true,
+  });
+  //@ts-ignore
+  t.is(res.result.status.SuccessValue, "");
+});
+
+test("Call approve bob", async (t) => {
+  const { root, contract, alice, bob } = t.context.accounts;
+
+  const identifyId = "testId";
+  await root.callRaw(contract, "set_operator", {
+    operator_address: alice.accountId,
+    value: true,
+  });
+  await alice.callRaw(contract, "approved_kyc", {
+    address: bob.accountId,
+    identifyId,
+  });
+
+  const checkKyc: any = await bob.call(contract, "get_my_kyc", {});
+  t.is(checkKyc.identifyId, identifyId);
+  t.is(checkKyc.isBlocked, false);
+});
